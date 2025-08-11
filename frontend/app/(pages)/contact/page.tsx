6915e84d1705cc52fd5e62df0707c38a6a2d7a39
theplaceincan <abrahatatsbeha@gmail.com>
2025-08-11 16:33:17 -0700

    created basic contact form setup

--- frontend/app/(pages)/contact/page.tsx
+++ frontend/app/(pages)/contact/page.tsx
@@ -2,14 +2,33 @@
 import Footer from "@/app/components/Footer"
 import Navbar from "@/app/components/Navbar"
 
+import styles from "./Contact.module.css"
+// className={styles["container"]}
 export default function About() {
   return (
     <>
-    <Navbar/>
-    <div>
-      <p>Contact</p>
-    </div>
-    <Footer/>
+      <Navbar />
+      <div className={styles["container"]}>
+        <div className={styles["title-section"]}>
+          <p className={styles["page-title"]}>Contact</p>
+        </div>
+        <div className={styles["form-container"]}>
+          <div className={styles["form-wrapper"]}>
+            <label className={styles["label"]}>Full name</label>
+            <input className={styles["input"]}></input>
+            <label className={styles["label"]}>Email</label>
+            <input className={styles["input"]}></input>
+            <label className={styles["label"]}>Phone number</label>
+            <input className={styles["input"]}></input>
+            <label className={styles["label"]}>Message</label>
+            <textarea className={styles["input"]}></textarea>
+            <div className={styles["btn-container"]}>
+              <button className={styles["btn"]}>Send Message</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   )
 }
